3b555c39c0d1c89cec6bafa3146f16f1836843dc
mohammed tifour <mohamedtifourt562@gmail.com>
2023-11-27 23:35:58 +0100

    handle errors

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const logger = require('./middlewares/logger/logger');
 const httpLogger = require('./middlewares/logger/httpLogger');
+const globalErrorHandler = require('./middlewares/errorHandler');
+const AppError = require('./utils/appError');
 
 dotenv.config();
 const app = express();
 
 app.use(httpLogger);
+
+app.use('*', (req, res, next) => {
+  next(new AppError(400, `can't find ${req.originalUrl}`));
+});
+app.use(globalErrorHandler);
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',

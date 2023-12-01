cd60233f8dcbfc50530625d6b40e3010a4262493
Mohammed Tifour <mohamedtifour562@gmail.com>
2023-12-01 19:49:07 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -14,6 +14,7 @@ const userSchema = mongoose.Schema(
       type: String,
       select: false,
     },
+    passwordChangedAt: Date,
     passwordResetToken: String,
     passwordResetExpires: Date,
   },
